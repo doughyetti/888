e1783ce2a50a6f9a49bdbe9f003122b10b40ca46
dough <dough@Dough-PC.localdomain>
2023-05-24 16:17:12 -0500

    meh

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,6 +2,13 @@
 const express = require('express');
 const router = express.Router();
 
+// backend/routes/index.js
+// ...
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+// ...
+
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
