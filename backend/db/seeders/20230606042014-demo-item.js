fd9af49449328fa2e185af2d56a4d6084f189889
dough <dough@Dough-PC.localdomain>
2023-06-08 16:26:46 -0500

    render

--- backend/db/seeders/20230606042014-demo-item.js
+++ backend/db/seeders/20230606042014-demo-item.js
@@ -18,14 +18,14 @@ module.exports = {
       },
       {
         menuId: 1,
-        name: "Vietnamese-style Egg Rolls",
+        name: "Vietnamese-Style Egg Rolls",
         description: "Pork, crispy skin, side of lime fish sauce.",
         price: 6.25
       },
       {
         menuId: 1,
-        name: "Steam Dumplings",
-        description: "(8 pieces), ground pork, scallions, cilantro, ginger.",
+        name: "Steam Dumplings (8 pieces)",
+        description: "Ground pork, scallions, cilantro, ginger.",
         price: 7.25
       },
       {
@@ -35,10 +35,33 @@ module.exports = {
         price: 16.00
       },
       {
+        menuId: 1,
+        name: "Bangkok Wings",
+        description: "Crispy wings, thai basil, yellow onion, tossed with Bangkok spicy tamarind sauce.",
+        price: 9.00
+      },
+      {
+        menuId: 1,
+        name: "Grilled Squid",
+        description: "Five flavors marinated grilled squid, side of sweet & tangy pineapple sauce.",
+        price: 7.75
+      },
+      {
         menuId: 2,
         name: "Pho Dac Biet",
         description: "Pho with combination of rare flank steak slices, beef tripe, braised brisket, meat ball, & soft tendon.",
         price: 13.00
+      },
+      {
+        menuId: 2,
+        name: "Pho Ga",
+        description: "Pho with chicken.",
+        price: 12.00
+      },{
+        menuId: 2,
+        name: "Pho Oxtail & Flank steak",
+        description: "Pho with combination of oxtail and flank steak slices.",
+        price: 16.00
       },
       {
         menuId: 2,
@@ -72,7 +95,7 @@ module.exports = {
       },
       {
         menuId: 3,
-        name: "Thai-style Stir-fried Seafood w/ Vegetables",
+        name: "Thai-Style Stir-fried Seafood w/ Vegetables",
         description: "Stir-fried shrimp, scallop, squid fish balls, and crab with thai basil, red & green bell peppers, broccoli, shiitake mushrooms, asparagus, carrot, and snow peas in chef's spicy thai sauce.",
         price: 17.00
       },
@@ -83,10 +106,22 @@ module.exports = {
         price: 17.25
       },
       {
+        menuId: 3,
+        name: "Panang Curry Lamb Chop",
+        description: "Grilled lamb chops, asparagus, red & green bell pepper, Kaffir lime leaf, yellow onion, in chef's coconut-milk panang curry sauce.",
+        price: 18
+      },
+      {
+        menuId: 3,
+        name: "Drunken Devil",
+        description: "Grilled shrimp wrapped in sliced flank steak with chef's sake - panang sauce, green & red bell peppers, yellow onion, and Kaffir lime leaf.",
+        price: 17.25
+      },
+      {
         menuId: 4,
-        name: "Hot Jasmine Tea",
-        description: "Fresh jasmine tea leaves brewed to perfection.",
-        price: 5.25
+        name: "Soda (free refills)",
+        description: "Mexican Cola ONLY uwu",
+        price: 3
       },
       {
         menuId: 4,
@@ -96,14 +131,26 @@ module.exports = {
       },
       {
         menuId: 4,
+        name: "Hot Jasmine Tea",
+        description: "Fresh jasmine tea leaves brewed to perfection.",
+        price: 4
+      },
+      {
+        menuId: 4,
         name: "888 Vietnamese Iced Coffee",
         description: "Rich, chocolatey flavor from the Vietnamese style robusta coffee, mixed with sweetened condensed milk.",
         price: 5.25
       },
       {
         menuId: 4,
+        name: "Avocado Milk Shake",
+        description: "Fresh avocado blended with milk and ice for heavenly deliciousness, uwu",
+        price: 5.25
+      },
+      {
+        menuId: 4,
         name: "Jackfruit Milk Shake",
-        description: "Fresh jackfruit blended with milk and ice for heavenly deliciousness, UWU",
+        description: "Fresh jackfruit blended with milk and ice for heavenly deliciousness, uwu",
         price: 5.95
       },
     ], {});
